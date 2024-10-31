196751e89dc30101be7f121e2d3b4571daf3d07a
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-10-31 22:20:11 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {StyledBtn} from "./components/Button.styled";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu>
+        <ul>
+          <li><a href=''>menu item 1</a></li>
+          <li><a href=''>menu item 2</a></li>
+          <li><a href=''>menu item 3</a></li>
+        </ul>
+      </Menu>
+      <Box>
+        <StyledBtn as={Link} href={'#'}>LinkComponent</StyledBtn>
+        <StyledBtn as={'a'} href={'#'}>Link</StyledBtn>
+        <StyledBtn>Hello</StyledBtn>
+        <SuperBtn>Super Button</SuperBtn>
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+
+
+const Link = styled.a`
+    color: #16166f;
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: transparent;
+    padding: 0;
+`
+
+const SuperBtn = styled(StyledBtn)`
+    border-radius: 10px;
+    background-color: #ffdd54;
+    color: #474747;
+`
+
+
+const Box = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+
+    button {
+        cursor: pointer;
+    }
+
+    ${Link} {
+        cursor: zoom-in;
+    }
+`
+
+const Menu = styled.nav`
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+
+        li > a {
+            color: forestgreen;
+        }
+
+        li + li {
+            margin-left: 15px;
+        }
+    }
+`
---
+++ src/components/Button.styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const StyledBtn = styled.button`
+    border: none;
+    background-color: #e158bc;
+    padding: 10px 20px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: bold;
+
+    &:last-child {
+        background-color: #2eff70;
+    }
+
+    &:hover {
+        background-color: #224695;
+    }
+
+`
